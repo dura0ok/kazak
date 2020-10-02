57e4e9de8f49bf0917a445575037d91891dd68e7
stepan7 <fenixrnd@mail.ru>
2020-10-02 13:49:47 +0300

    edit naming from footer and title

--- resources/views/layouts/base/template.blade.php
+++ resources/views/layouts/base/template.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Всевеликое Войско Донское</title>
+    <title>Городское казачье общество «Ростовское»</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     @stack('styles')
 </head>
