b4e99b9e489b55bc82043d02555fd696055316ae
stepan7 <fenixrnd@mail.ru>
2020-10-06 20:43:48 +0300

    fix link to news(slide)

--- resources/views/admin/slides/index.blade.php
+++ resources/views/admin/slides/index.blade.php
@@ -9,7 +9,7 @@
                 <h1>{{ $slide->title }}</h1>
                 <img src="{{ asset('storage/'.$slide->image) }}" alt="{{ $slide->title }}">
                 <p>{{ $slide->description }}</p>
-                @if($slide->article_id != null) <a href="" class="parent">Перейти на родительскую новость</a>@endif
+                @if($slide->article_id != null) <a href="{{ route('article', $slide->article_id) }}" class="parent">Перейти на родительскую новость</a>@endif
                 <div class="buttons">
                     <a href="{{ route('slides.edit', $slide->id) }}" class="yellow_btn">Редактировать</a>
                     <form action="{{ route('slides.destroy', $slide) }}" method="POST">
